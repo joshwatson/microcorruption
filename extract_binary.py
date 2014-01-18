554d8e76453d371975115d7ac3f70d807bc548db
Josh Watson <josh@joshwatson.com>
2014-01-18 17:25:35 -0500

    decided to go with ascii, for pasting into the website's disassembler

--- extract_binary.py
+++ extract_binary.py
@@ -6,9 +6,7 @@ def main():
 	with open(output_file, 'wb') as outF:
 		with open(input_file, 'r') as inF:
 			for line in inF:
-				for i in xrange(11,26,5):
-					bytes = ''.join(map(chr, [int(line[i+2:i+4], 16), int(line[i:i+2], 16)]))
-					outF.write(bytes)
+				outF.write(line[11:31])
 					
 			
 
